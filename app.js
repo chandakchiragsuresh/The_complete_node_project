8bf5c5f55ea2c529ed575a62f54569503838b783
chandakchiragsuresh <chandakchiragsuresh@gmail.com>
2022-04-18 23:18:28 +0530

    'added_error_controller'

--- app.js
+++ app.js
@@ -1,19 +1,16 @@
 const express = require('express')
 const admin = require('./routes/admin')
 const adminRouter = admin.router
-
-
 const shopRouter = require('./routes/shop')
-
 const app = express()
 const path = require('path')
 const pug = require('pug')
+const errorController = require('/The_Complete_NodeJS_Maximilian Schwarzmuller/controllers/errorController')
 
 
 app.set('view engine', 'pug')//it will set global variable of node
 
 app.set('views', 'views')//here first views is our global variable and we are setting up to views folder
-
 
 app.use(express.static(path.join(__dirname, 'public')))
 
@@ -21,11 +18,7 @@ app.use('/admin',adminRouter)
 
 app.use(shopRouter)
 
-
-app.use((req, res)=>{
-    res.render('404')
-})
-
+app.use(errorController.getError)
 
 app.listen(3000, ()=>{
     console.log('Server is up on port 3000!')
---
+++ controllers/errorController.js
@@ -0,0 +1,5 @@
+const path = require('path')
+
+exports.getError = (req, res)=>{
+    res.render('404')
+}
